98cba39bc5ae896b981df815b61fff5188e9d7c9
lamin <lamin@10.157.255.141>
2021-11-03 18:04:34 -0400

    changes- for call

--- WingVelocity.m
+++ WingVelocity.m
@@ -1,14 +1,19 @@
 %Estimation of wing velocity
-function Vw = Vw_P(r,c)                           % Free stream velocity on planform frame with respect to wing spar length and chord length              
+function Vw = WingVelocity(r,c)                           % Free stream velocity on planform frame with respect to wing spar length and chord length              
 
 % Definitions: B-Body Frame; S-Spar Frame; P- Planform Frame; wr- Wing Root; ws-Wing Spar;
 % wp-Wing planform; 
 
  r_C.G = [0 0 0];        % Centre of gravity of body frame.
- r_WR = [ ];             % origin of wing root/ positoon vector of wing root
+ 
+ %Ixx = 
+ %Iyy =
+ %Izz = 
+ % I_B = [Ixx Iyy Izz]     % Moment of inertia of body frame in the X, Y and Z axes
+ r_WR = [Izz];           % origin of wing root/ position vector of wing root
  t = ;                   % estimated flight time
  
- R_I_B(TH)= ;                                           % TH- standard rotation angle theta
+ % R_I_B(TH)= ;                                           % TH- standard rotation angle theta
  R1 = [1 0 0; 0 cos TH sin TH; 0 -sin TH cos TH];
  R2 = [cos TH 0 -sin TH; 0 1 0; sin TH 0 cos TH];
  R3 = [cos TH sin TH 0; sin TH cos TH 0; 0 0 1]; 
@@ -23,6 +28,7 @@ function Vw = Vw_P(r,c)                           % Free stream velocity on plan
  % Wing movements definition 
  u= 1;    % upstroke 
  d = -1;  % downstroke 
+ % left wing and right wing definition
  
 % Position vector of infinitesimal point on body frame(r_B); spar frame (r_S), and planform (r_P)
 r_P = [0; r; -c];                           %Position vector of infinitesimal point on wing planform (r_P)
@@ -31,8 +37,8 @@ r_S = R_P_S * r_P;                          %Position vector of infinitesimal po
 
 % Velocity due to rigid body rotation
 
-Wb_B = transpose(R_I_B);                % angular velocity of body frame with reepect to chaning theta with time                   % 
-V_r = @ (R_WR_S) * Wb_B * r_B;          % FINAL 1
+Wb_B = transpose(R_I_B);                % angular velocity of body frame with respect to changng theta with time                   % 
+V_r = @(s, phi) R_WR_S(s, phi)) * Wb_B * r_B;          % FINAL 1
 
 if s = 1 & u = 1 
 V_r_u = @(s, phi) R_WR_S(s, phi))* u * Wb_B * r_B;  % right and left wing upstroke
@@ -42,23 +48,22 @@ if s = -1 & d = -1
 V_r_d = @(s, phi) R_WR_S(s, phi)* d * Wb_B * r_B;  % right and left wing downstroke 
 end 
 
-
 % Velocity due to rigid body translation
 
- r_WR_CG = r_WR - r_CG;    % position vector for centre of gravity of rigid body to wing root
- Vb =                      % Linear velocity of rigid body in body frame (should be constantly changing)
+ r_WR_CG = r_WR - r_CG;    % position vector for centre of gravity of rigid body to wing root on body frame
+ Vb =[ ]                  % Linear velocity of rigid body in body frame (should be constantly changing)- as an input
  
- V_b_B = Vb + (Wb_B * r_WR_CG)       % evaluation of rigid body velocity 
+ V_b_B = Vb + (Wb_B * r_WR_CG)       % evaluation of rigid body velocity of body frame
  
  V_t = R_B_S * V_b_B;                % FINAL 2
 
 
 %Velocity due to wing flapping
-dR_WR_S = diff (@(s, phi) R_WR_S(s, phi), t);      % differential with respect to time of wing root with respect to time in spar frame
+% dR_WR_S = diff (@(s, phi) R_WR_S(s, phi), t);      % differential with respect to time of wing root with respect to time in spar frame
 
 V_f = @(s, phi) R_WR_S(s, phi) * dR_WR_S * r_S;                   % FINAL 3
 
-Vw = V_r + V_t + V_f;                 % Free stream velocity over an incremental area of wing planform 
+Vw = V_r + V_t + V_f;                 % Free stream velocity over an incremental area of wing planform (Addition of FINAL 1 to 3) 
 
 
 
