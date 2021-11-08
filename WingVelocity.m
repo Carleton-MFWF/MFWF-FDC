da001e8922b88b4977b8310c56621de970e88003
lamin <lamin@192.168.0.22>
2021-11-08 00:42:51 -0500

    edit to wingVelocity script file

--- WingVelocity.m
+++ WingVelocity.m
@@ -6,14 +6,11 @@ function Vw = WingVelocity(r,c)                           % Free stream velocity
 
  r_C.G = [0 0 0];        % Centre of gravity of body frame.
  
- %Ixx = 
- %Iyy =
- %Izz = 
- % I_B = [Ixx Iyy Izz]     % Moment of inertia of body frame in the X, Y and Z axes
- r_WR = [Izz];           % origin of wing root/ position vector of wing root
- t = ;                   % estimated flight time
+  
+ r_WR = [Izz];           %  origin of wing root/ position vector of wing root
+ t = ;                   % estimated flight time- probably input
  
- % R_I_B(TH)= ;                                           % TH- standard rotation angle theta
+ % R_I_B(TH)= ;  input = theta                                         % TH- standard rotation angle theta
  R1 = [1 0 0; 0 cos TH sin TH; 0 -sin TH cos TH];
  R2 = [cos TH 0 -sin TH; 0 1 0; sin TH 0 cos TH];
  R3 = [cos TH sin TH 0; sin TH cos TH 0; 0 0 1]; 
@@ -32,6 +29,7 @@ function Vw = WingVelocity(r,c)                           % Free stream velocity
  
 % Position vector of infinitesimal point on body frame(r_B); spar frame (r_S), and planform (r_P)
 r_P = [0; r; -c];                           %Position vector of infinitesimal point on wing planform (r_P)
+ % currently not sur- possibly taking an integral over r & c
 r_B = (r_CG - r_WR)+ R_WR_S * R_S_P * r_P;  %Position vector of infinitesimal point on rigid body (r_B)
 r_S = R_P_S * r_P;                          %Position vector of infinitesimal point on spar frame (r_S)
 
@@ -51,7 +49,7 @@ end
 % Velocity due to rigid body translation
 
  r_WR_CG = r_WR - r_CG;    % position vector for centre of gravity of rigid body to wing root on body frame
- Vb =[ ]                  % Linear velocity of rigid body in body frame (should be constantly changing)- as an input
+ Vb =[ ]                  % Linear velocity of rigid body in body frame - as an input
  
  V_b_B = Vb + (Wb_B * r_WR_CG)       % evaluation of rigid body velocity of body frame
  
@@ -63,7 +61,7 @@ end
 
 V_f = @(s, phi) R_WR_S(s, phi) * dR_WR_S * r_S;                   % FINAL 3
 
-Vw = V_r + V_t + V_f;                 % Free stream velocity over an incremental area of wing planform (Addition of FINAL 1 to 3) 
+Vw = V_r + V_t + V_f;                 % Free stream velocity over an incremental area of wing planform (Addition of FINAL 1 to FINAL 3) 
 
 
 
