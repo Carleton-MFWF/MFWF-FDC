d7f294eed073899808edc6fd8f0c81af07911347
lamin <olaoluwalamina@cmail.carleton.ca>
2022-02-02 20:52:10 -0500

    Update

--- plant/rigid_body_model.m
+++ plant/rigid_body_model.m
@@ -5,19 +5,34 @@ function [w, v] = Forces (F, M)
 m = 0.04;                     % mass of MFWF in kg
 g = 9.81;                   % acceleration due to gravity (in m/s2)
 
+W_Component =
+
 x_b = 0.05;                       % Breadth of body frame (in metres)
 y_b = 0.25;                       % Length of body frame (in metres)
 z_b = 0.15;                       % Height of flyer (in metres)
+ 
+syms tx                        % theta 
+syms ty                        % theta
+syms tz                        % theta
 
-syms p                         % stroke angle 
-syms t                         % theta 
-syms a                         % alpha
-
-phi = 30;
-theta = 30;
+tx = 30;
+ty = 30;
+tz = 30;
 
 % Rotation matrice from inertial frame to body frame
-R_I_B = [ 
+R_1 = [1 0 0; 
+    0 cos(tx) sin(tx);
+    0  -sin(tx) cos(tx)];
+
+R_2 = [cos(ty) 0 -sin(ty);
+       0 1 0;
+      sin(ty) 0 cos(ty)];
+    
+R_3 = [cos(tz) sin(tz) 0;
+    sin(tz) cos(tz) 0;
+    0 0 1];
+
+R_I_B = R_1 * R_2 * R_3;
 
 % Mass Moment of Inertia across x,y and z axes
 I_xx = (square(y_b) + square(z_b)) * m;
@@ -28,6 +43,8 @@ I_zz = (square(x_b) + square(y_b)) * m;
 I = [I_xx  0  0;
      0  I_yy  0;
      0   0  I_zz];
+ 
+ 
      
      
      
