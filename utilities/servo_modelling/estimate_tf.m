6e96b85d7b7fa0058d7d430aaa27a2cc19c4afd6
Nabil Roberts <Nabilkroberts@gmail.com>
2022-02-03 18:09:21 -0500

    integrate servo model into plant

--- utilities/servo_modelling/estimate_tf.m
+++ utilities/servo_modelling/estimate_tf.m
@@ -12,3 +12,26 @@ data = iddata(T.y,T.u,Ts);
 sys = tfest(data,2)
 
 compare(data,sys);
+% Simulink.data.dictionary.create('./data/servo.sldd')
+dataDict =  Simulink.data.dictionary.open('servo.sldd');
+
+
+%%
+[num, den] = tfdata(sys, 'v');
+
+designData = getSection(dataDict,'Design Data');
+
+if exist(designData,'tf_num')
+      tfObj = designData.getEntry('tf_num');
+      tfObj.setValue(num);
+else
+    tfObj = designData.addEntry('tf_num', num);
+end
+
+if exist(designData,'tf_den')
+      tfObj = designData.getEntry('tf_den');
+      tfObj.setValue(den);
+else
+    tfObj = designData.addEntry('tf_den', den);
+end
+dataDict.saveChanges();
