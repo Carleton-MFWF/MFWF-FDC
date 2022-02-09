f0d5aa5c8a9cd5500c4f26b6fef9ed2542f0ea62
lamin <olaoluwalamina@cmail.carleton.ca>
2022-02-09 09:46:31 -0500

    Script file completed, simulink implementation in-progress

--- plant/rigid_body_model.m
+++ plant/rigid_body_model.m
@@ -1,11 +1,167 @@
-%function [v_b w_b] = Forces (F, M)
-
 %Rigid Body Model
 
-m = 0.04;                     % mass of MFWF in kg
+% The system is modelled as an ODE, with q defined as a 12- element state
+% vector
+
+function qDOT = rigid_body_model (t, q, m, I, FM_at_t)
+
 g = 9.81;                   % acceleration due to gravity (in m/s2)
 
-%W_Component = [0; 0; -g];
+% extracting forces and moments
+FM = FbMb_at_t(t);
+F = FM(1:3);            % Force vector
+M = FM(4:6);            % Moment vector
+
+% extract components from STATE Vector
+v_B   = q(1:3);           % (m/s), translational velocity
+w     = q(4:6);           % (rad/s), angular velocity
+e     = q(7:9);           % (rad/s), euler angles
+PosI = q(10:12);         % (m), Inertia frames position
+
+% Force equation, F = m * V_B + m * w_x_v) Translational equation of motion
+vDOT = F/M - cross(w, v_B);
+
+% Moment equation, M = I * wDOT + w_x_(I * w) Rotational equation of motion
+wDOT = inv(I) * (M - cross(w, I * w));
+
+% euler rates from body rates
+eDOT = LOC_get_eDOT(w,e);
+
+%Inertial Velocity
+R_I_B = LOC_get_R_I_B(e);
+B_I_R = R_I_B.';
+PosIDOT = B_I_R * [0; 0; g] * v_B ;        
+
+% assembling the final derivative vector
+qDOT = [vDOT;
+        wDOT; 
+        eDOT;
+        PosIDOT];
+
+end
+
+function R_I_B = LOC_get_R_I_B(e)
+
+ph = e(1);
+th = e(2);
+ps = e(3);
+
+R_I_B = [cos(th)*cps              cos (th)*sps              -sin(th);
+               sin(ph)*sin(th)*cos(ps)-cos(ph)*sin(ps)  sin(ph)*sin(th)*sin(ps)+cos(ph)*cos(ps)  sin(ph)*cos(th);
+               cos(ph)*sin(th)*cos(ps)+sin(ph)*sin(ps)  cos(ph)*sin(th)*sin(ps)-sin(ph)*cos(ps)  cos(ph)*cos(th)];
+end
+
+function euler_rates = LOC_get_eDOT(w,e)
+ p= w(1);
+ q = w(2);
+ r = w(3);
+ 
+ ph = e(1);
+ th = e(2);
+ ps = e(3);
+
+euler_rates = [(r.* cos (ps) + q.* sin(ps))./cos(th);
+                q.* cos(ps) - r.* sin(ps);
+                (p.* cos(th) + r.* cos(ps).* sin(th) + q.* sin(ps).* sin(th))./cos(th)];
+end
+  
+% Defining Flyer Mass, Inertia and Initial Conditions
+
+%Inertia Tensor (assumption of perfect plane of symmetry) (kg.m^2)
+%I_xx = 0.0034;
+%I_yy = 0.001;
+%I_zz = 0.0026;
+
+I = [0.0034  0  0;
+     0  0.001  0;
+     0   0  0.0026];
+ 
+ %Flyer Initial Conditions
+ v_B_Initial = [0;0;0];     % (m/s) initial velocity in body axes
+ w_Initial = [0;0;0];       % (rad/s) initial body rates
+ e_Initial = [0;0;0];       % (rad) initial euler angles
+ PosI_Initial = [0;0;0];    % (m) initial position in inertial axes
+ 
+ % state vector initial conditions
+ 
+ q_Initial = [VDOT_Initial;
+                wDOT_Initial;
+                eDOT_Initial;
+                PosIDOT_Initial];
+            
+%Solving the System
+% FM_at_t = @(t)     to get input of forces and moments
+dqdt_at_t = @ (t,q) rigid_body_model(t, q, m, I, FM_at_t);
+
+%defining some ODE solver settings
+t_span = [0 60];    % sec, [tstart tend]
+my_options = odeset('ReTol', 1e-7, 'AbsTol', 1e-7);          % seconds
+
+% using ODE solver
+[T, Q] = ode45(dqdt_at_t, tspan, q_Initial, my_options);
+
+figure 1;
+plot(T,Q, 'b')
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%{ 
+W_Component = [0; 0; -g];
 
 x_b = 0.05;                       % Breadth of body frame (in metres)
 y_b = 0.25;                       % Length of body frame (in metres)
@@ -56,27 +212,9 @@ I = [I_xx  0  0;
  My = -0.0562;
  Mz = 0.0938;
  
- syms v_b_x  v_b_y  v_b_z  w_b_x  w_b_y  w_b_z;
- 
- w_b_x = v_b_x;
- w_b_y = v_b_y;
- w_b_z = v_b_z; 
- 
- eq1 = (0.4 * v_b_x) + 0.4 * ((w_b_y * v_b_z)-(w_b_z * v_b_y)) - Fx == 0;
- eq2 = (0.4 * v_b_y) + 0.4 * ((w_b_z * v_b_x)-(w_b_x * w_b_z)) - Fy == 0;
- eq3 = (0.4 * v_b_z) + 0.4 * ((w_b_x * v_b_y) - (w_b_y * v_b_x)) - Fz == 0;
- eq4 = (I_xx * w_b_x) + (w_b_x * (I_yy - I_zz) * w_b_x) - Mx == 0;
- eq5 = (I_yy * w_b_y) + (w_b_y * (I_zz - I_xx) * w_b_y) - My == 0;
- eq6 = (I_zz * w_b_z) + (w_b_z * (I_xx - I_yy)* w_b_z) - Mz == 0;
- 
- Eq = [eq1, eq2, eq3, eq4, eq5, eq6];
- vars = [v_b_x, v_b_y, v_b_z, w_b_x, w_b_y, w_b_z];
- sol = solve(Eq, vars);
-
- A = double([sol.v_b_x; sol.v_b_y; sol.v_b_z; sol.w_b_x; sol.w_b_y; sol.w_b_z])
  
  %Method 2: Uising first order non-linear oDE
- % Moment in x-direction
+% Moment in x-direction
 % Moment in x-direction
 tspan = [0 5];
 x0 = 0.5;
@@ -112,8 +250,149 @@ xlabel('Time (s)')
 ylabel ('Angular velocity (rad/s, E-14)')
 title('Instantenous angular velocity vs time')
 %legend('w_x', 'w_y', 'w_z', 'Location', 'SW')
+ %}
 
-     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% syms v_b_x  v_b_y  v_b_z  w_b_x  w_b_y  w_b_z;
+ 
+% w_b_x = v_b_x;
+% w_b_y = v_b_y;
+% w_b_z = v_b_z; 
+ 
+% eq1 = (0.4 * v_b_x) + 0.4 * ((w_b_y * v_b_z)-(w_b_z * v_b_y)) - Fx == 0;
+% eq2 = (0.4 * v_b_y) + 0.4 * ((w_b_z * v_b_x)-(w_b_x * w_b_z)) - Fy == 0;
+% eq3 = (0.4 * v_b_z) + 0.4 * ((w_b_x * v_b_y) - (w_b_y * v_b_x)) - Fz == 0;
+% eq4 = (I_xx * w_b_x) + (w_b_x * (I_yy - I_zz) * w_b_x) - Mx == 0;
+% eq5 = (I_yy * w_b_y) + (w_b_y * (I_zz - I_xx) * w_b_y) - My == 0;
+% eq6 = (I_zz * w_b_z) + (w_b_z * (I_xx - I_yy)* w_b_z) - Mz == 0;
+ 
+% Eq = [eq1, eq2, eq3, eq4, eq5, eq6];
+% vars = [v_b_x, v_b_y, v_b_z, w_b_x, w_b_y, w_b_z];
+% sol = solve(Eq, vars);
+
+% A = double([sol.v_b_x; sol.v_b_y; sol.v_b_z; sol.w_b_x; sol.w_b_y; sol.w_b_z])%}     
      
      
      
